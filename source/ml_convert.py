9a665e41121854cc7c2de64e0c739415e565660e
Filip Gulan <filip.gulan@infinum.hr>
2017-10-01 10:13:30 +0200

    Updated ml_convert.py

--- source/ml_convert.py
+++ source/ml_convert.py
@@ -1,11 +1,24 @@
+#!/usr/local/bin/python
+# -*- coding: utf-8 -*-
 import sys
 import os
 import coremltools
 
-
-# Convert a caffe model to a classifier in Core ML
-coreml_model = coremltools.converters.keras.convert(('./10_000_twohidden/model.json', './10_000_twohidden/model.h5'))
-
-
-# Now save the model
-coreml_model.save('LetterClass.mlmodel')
+model_data = ('./10_000_twohidden/model.json', './10_000_twohidden/model.h5')
+image_scale = 1/255.
+class_labels = ['A', 'B', 'C', 'Č', 'Ć', 'D', 'Đ', 'E',
+                'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+                'N', 'O', 'P', 'R', 'S', 'Š', 'T', 'U',
+                'V', 'Z', 'Ž', 'X', 'Y', 'W', 'Q', '-']
+coreml_model = coremltools.converters.keras.convert(model_data,
+                                                   input_names='image',
+                                                   image_input_names='image',
+                                                   output_names = ['letter'],
+                                                   class_labels = class_labels,
+                                                   image_scale = image_scale)
+coreml_model.author = 'Filip Gulan'
+coreml_model.license = 'FER'
+coreml_model.short_description = 'Model used for classifying Croatian hand written letters'
+coreml_model.input_description['image'] = 'Grayscale image 30x30 of hand written letter'
+coreml_model.output_description['letter'] = 'Predicted letter'
+coreml_model.save('LetterClass_image.mlmodel')
